58803673e26ab49c6a9f35ff64d5f163f800b6c2
chakbun <cheungjaben@hotmail.com>
2015-11-03 14:18:26 +0800

    add watch target

---
+++ Afternoon4Watch Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Afternoon4Watch Extension
+//
+//  Created by Jaben on 15/11/3.
+//  Copyright © 2015年 After. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ Afternoon4Watch Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Afternoon4Watch Extension
+//
+//  Created by Jaben on 15/11/3.
+//  Copyright © 2015年 After. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
